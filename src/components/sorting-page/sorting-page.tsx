ea0212f312d8243778abce83087e4806828b291e
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-02-18 16:27:31 +0300

    18/2/23

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import SortingPageStyles from "./sorting-page.module.css";
@@ -8,33 +8,166 @@ import { delay } from "../../utils";
 import { Column } from "../ui/column/column";
 import { ElementStates } from "../../types/element-states";
 import { DELAY_IN_MS } from "../../constants/delays";
-
+import { initial, minLEN, maxLEN, randomIntFromInterval, swap, desc, asc, array } from "./utils-sorting-page";
 import { IRandomArr } from "../../types/sorting-page";
 
 
 
 
 export const SortingPage: React.FC = () => {
+
+  const [radioOption, setRadioOption] = useState<string>("selection-sort");
+  const [isLoader, setIsLoader] = useState<string>(initial);
+  const [randomArray, setRandomArray] = useState<IRandomArr[]>([]);
+
+  const randomArr = (min: number, max: number, minLen = minLEN, maxLen = maxLEN): IRandomArr[] => {
+    setIsLoader('array')
+    const length: number = randomIntFromInterval(minLen, maxLen);
+    const lotteryNumbers: IRandomArr[] = [];
+
+    for (let i = 0; i <= length - 1; i++) {
+      lotteryNumbers.push({
+        num: randomIntFromInterval(min, max),
+        state: ElementStates.Default
+      });
+    }
+    return lotteryNumbers;
+  }
+
+  useEffect(() => {
+    setRandomArray(randomArr(0, 100))
+  }, []);
+
+  const onClickNewArray = (): void => {
+    setRandomArray(randomArr(0, 100));
+  }
+
+  const onChangeRadio = (e: ChangeEvent<HTMLInputElement>): void => {
+    setRadioOption((e.target as HTMLInputElement).value);
+  };
+
+  const selectionSort = async (array: IRandomArr[], direction: boolean): Promise<IRandomArr[]> => {
+    const { length } = array;
+    for (let i = 0; i < length; i++) {
+      let maxInd = i;
+      array[maxInd].state = ElementStates.Changing;
+      for (let j = i + 1; j < length; j++) {
+        array[j].state = ElementStates.Changing;
+        setRandomArray([...array]);
+        await delay(DELAY_IN_MS);
+        if (direction ? array[j].num < array[maxInd].num : array[j].num > array[maxInd].num) {
+          maxInd = j;
+          array[j].state = ElementStates.Changing;
+          array[maxInd].state = i === maxInd ? ElementStates.Changing : ElementStates.Default;
+        }
+        if (j !== maxInd) {
+          array[j].state = ElementStates.Default;
+        }
+        setRandomArray([...array]);
+      }
+      swap(array, maxInd, i);
+      array[maxInd].state = ElementStates.Default;
+      array[i].state = ElementStates.Modified;
+      setRandomArray([...array]);
+    }
+    setIsLoader('default');
+    return array;
+  };
+
+  const bubbleSort = async (array: IRandomArr[], direction: boolean): Promise<IRandomArr[]> => {
+    const { length } = array;
+    for (let i = 0; i < length; i++) {
+      for (let compareIndex = 0; compareIndex < length - i - 1; compareIndex++) {
+        const left = array[compareIndex].num;
+        const right = array[compareIndex + 1].num;
+        array[compareIndex].state = ElementStates.Changing;
+        array[compareIndex + 1].state = ElementStates.Changing;
+        setRandomArray([...array]);
+        await delay(DELAY_IN_MS);
+        if (direction ? left > right : left < right) {
+          array[compareIndex].num = right;
+          array[compareIndex + 1].num = left;
+        }
+        array[compareIndex].state = ElementStates.Default;
+        if (array[compareIndex + 1]) {
+          array[compareIndex + 1].state = ElementStates.Default;
+        }
+        setRandomArray([...array]);
+      }
+      array[array.length - i - 1].state = ElementStates.Modified;
+      setRandomArray([...array])
+    }
+    setIsLoader('default');
+    return array;
+  }
+
+  const onClickSort = async (direction: string): Promise<void> => {
+    setIsLoader(direction);
+    const compare = direction === 'Direction.Ascending';
+    if (radioOption === 'selection-sort') {
+      setRandomArray([...await selectionSort(randomArray, compare)]);
+    } else {
+      setRandomArray([...await bubbleSort(randomArray, compare)]);
+    }
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
       <form className={SortingPageStyles.form}>
         <div className={SortingPageStyles.container}>
           <RadioInput
-            label="Выбор" />
+            label="Выбор"
+            name={"sorting-type"}
+            value={"selection-sort"}
+            defaultChecked
+            extraClass="mr-20"
+            onChange={onChangeRadio}
+            disabled={isLoader === desc || isLoader === asc}
+          />
 
           <RadioInput
             label="Пузырек"
+            name={"sorting-type"}
+            value={"selection-sort"}
+            defaultChecked
+            onChange={onChangeRadio}
+            disabled={isLoader === desc || isLoader === asc}
           />
         </div>
         <div className={SortingPageStyles.container}>
           <Button
-            text="По возрастанию" />
+            text="По возрастанию"
+            extraClass="mr-6"
+            onClick={() => onClickSort(asc)}
+            isLoader={isLoader === asc}
+            disabled={isLoader === desc}
+            sorting={Direction.Ascending}
+          />
           <Button
-            text="По убыванию" />
-            <Button
-            text="Новый массив"/>
+            text="По убыванию"
+            extraClass="mr-40"
+            onClick={() => onClickSort(desc)}
+            sorting={Direction.Ascending}
+            isLoader={isLoader === desc}
+            disabled={isLoader === asc}
+          />
+          <Button
+            text="Новый массив"
+            onClick={onClickNewArray}
+            disabled={isLoader !== array && isLoader !== initial}
+          />
         </div>
       </form>
+      <ul className={SortingPageStyles.list}>
+                {randomArray.map((element: IRandomArr, index: number, state: IRandomArr[]) => {
+                    return (
+                        <Column
+                            key={index}
+                            index={element.num}
+                            state={element.state}
+                        />)
+                })}
+            </ul>
 
     </SolutionLayout>
   );
---
+++ src/components/sorting-page/utils-sorting-page.ts
@@ -0,0 +1,50 @@
+import { IRandomArr } from "../../types/sorting-page";
+
+export const minLEN: number = 3;
+export const maxLEN: number = 17;
+export const array: string = 'array';
+export const initial: string = 'default';
+export const desc: string = 'Direction.Descending';
+export const asc: string = 'Direction.Ascending';
+
+export const randomIntFromInterval = (min: number, max: number): number => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const swap = (arr: IRandomArr[], firstIndex: number, secondIndex: number): void => {
+	const temp = arr[firstIndex];
+	arr[firstIndex] = arr[secondIndex];
+	arr[secondIndex] = temp;
+};
+
+export const selectionSort = (array: IRandomArr[]) => {
+	const { length } = array;
+	for (let i = 0; i < length; i++) {
+		let maxInd = i;
+		for (let j = i + 1; j < length; j++) {
+			if (array[maxInd].num < array[j].num) {
+				maxInd = j;
+			}
+		}
+		if (maxInd !== i) {
+			swap(array, i, maxInd);
+		}
+	}
+	return array;
+};
+
+export const bubbleSort = (array: IRandomArr[]) => {
+	const { length } = array;
+	for (let i = 0; i < length; i++) {
+		for (let compareIndex = 0; compareIndex < length - i - 1; compareIndex++) {
+			const left = array[compareIndex].num;
+			const right = array[compareIndex + 1].num;
+			if (left < right) {
+				array[compareIndex].num = right;
+				array[compareIndex + 1].num = left;
+			}
+		}
+	}
+
+	return array;
+}
