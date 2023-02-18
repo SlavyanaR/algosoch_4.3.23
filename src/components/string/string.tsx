3dec1b450203dae5f756325b49454b459436ece3
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-02-18 16:47:14 +0300

    18/2/23/string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import StringStyles from "./string.module.css"
+import { swap } from "./utils-string";
+import { DELAY_IN_MS } from "../../constants/delays";
+import { delay } from "../../utils";
 
 export const StringComponent: React.FC = () => {
+
+  const [isLoader, setIsLoader] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [reversArray, setReversArray] = useState<Array<string>>([]);
+
+  const reversString = async (string: string): Promise<string[]> => {
+    const arrayLetters = string.split('');
+    let end = arrayLetters.length;
+
+    setCurrentIndex(0);
+    setIsLoader(true);
+    setReversArray([...arrayLetters]);
+    await delay(DELAY_IN_MS);
+
+    for (let i = 0; i < Math.floor(end / 2); i++) {
+      swap(arrayLetters, i, end - 1);
+      setCurrentIndex(i => i + 1);
+      setReversArray([...arrayLetters]);
+      await delay(DELAY_IN_MS);
+    }
+
+    setCurrentIndex(i => i + 1);
+    setIsLoader(false);
+
+    return arrayLetters;
+  }
+
+  const onChange = (e: FormEvent<HTMLInputElement>): void => {
+    const string = e.currentTarget.value.trim();
+    setInputValue(string);
+  }
+
+  const onClickRevers = (e: FormEvent<HTMLFormElement> | FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    reversString(inputValue);
+    setInputValue('');
+  }
+
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={StringStyles.form}>
+        <Input
+          extraClass="mr-6"
+          isLimitText={true}
+        />
+        <Button
+          text="Развернуть"
+        />
+      </form>
     </SolutionLayout>
   );
 };
---
+++ src/components/string/utils-string.ts
@@ -0,0 +1,31 @@
+import { ElementStates } from "../../types/element-states";
+
+export const maxLEN = 11;
+
+
+export const stateCircle = (index: number, currIndex: number, arr: Array<string | number>) => {
+	let arrLength = arr.length - 1
+	if (currIndex < index || currIndex > arrLength - index) {
+		return ElementStates.Modified
+	}
+	if (currIndex === index || currIndex === arrLength - index) {
+		return ElementStates.Changing
+	}
+	return ElementStates.Default
+}
+
+export const swap = (arr: string[], firstIndex: number, secondIndex: number): void => {
+	[arr[firstIndex], arr[secondIndex - firstIndex]] = [arr[secondIndex - firstIndex], arr[firstIndex]];
+};
+
+
+export function reversString (string: string) {
+	const arrayOfLetters = string.split('');
+	let end = arrayOfLetters.length;
+
+	for (let i = 0; i < Math.floor(end / 2); i++) {
+		swap(arrayOfLetters, i, end - 1);
+	}
+
+	return arrayOfLetters;
+}
