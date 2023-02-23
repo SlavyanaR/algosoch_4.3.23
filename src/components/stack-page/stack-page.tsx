3e4fb75c738641355452ae21ccd2dd2cd07c1f75
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-02-23 13:09:27 +0300

    23/2/23

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Button } from "../ui/button/button";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
+<form>
+  <div>
+    <Input/>
+    <Button
+    text="Добавить"
+    />
+    <Button
+    text="Удалить"
+    />
+  </div>
+  <Button
+    text="Очистить"
+  />
+</form>
+
 
     </SolutionLayout>
   );
